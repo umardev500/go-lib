7597a15118489893728c1c5d8158a0da94ae45b4
umardev500 <umar.dev500@gmail.com>
2023-09-29 05:33:32 +0700

    add return for update bson

--- struct.go
+++ struct.go
@@ -1,6 +1,7 @@
 package golib
 
 import (
+	"fmt"
 	"reflect"
 	"strings"
 
@@ -8,7 +9,40 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-func structToBson(elements reflect.Value, tag string) bson.D {
+func regStructToBson(elements reflect.Value, parentFieldTag string, result *bson.D, isUpdate bool) {
+	tag := "bson"
+	total := elements.NumField()
+	elemTypes := elements.Type()
+	var out bson.D
+
+	if isUpdate {
+		for i := 0; i < total; i++ {
+			field := elemTypes.Field(i)
+			if !field.IsExported() {
+				continue
+			}
+			fieldTag := strings.Split(field.Tag.Get(tag), ",")[0]
+			fieldValue := elements.Field(i)
+			fieldZero := reflect.Zero(fieldValue.Type())
+			// check for not zero
+			if reflect.DeepEqual(fieldZero.Interface(), fieldValue.Interface()) {
+				continue
+			}
+			if isUpdate {
+				key := fmt.Sprintf("%s.%s", parentFieldTag, fieldTag)
+				out = append(out, primitive.E{Key: key, Value: fieldValue.Interface()})
+				continue
+			}
+		}
+	} else {
+		o := structToBson(elements, isUpdate, tag)
+		out = append(out, primitive.E{Key: parentFieldTag, Value: o})
+	}
+
+	*result = append(*result, out...)
+}
+
+func structToBson(elements reflect.Value, isUpdate bool, tag string) bson.D {
 	total := elements.NumField()
 	elemTypes := elements.Type()
 	var out bson.D
@@ -26,8 +60,7 @@ func structToBson(elements reflect.Value, tag string) bson.D {
 		if !reflect.DeepEqual(fieldZero.Interface(), fieldValue.Interface()) {
 			// check for struct
 			if fieldValue.Kind() == reflect.Struct {
-				o := structToBson(fieldValue, tag)
-				out = append(out, primitive.E{Key: fieldTag, Value: o})
+				regStructToBson(fieldValue, fieldTag, &out, isUpdate)
 				continue
 			}
 
@@ -42,8 +75,7 @@ func structToBson(elements reflect.Value, tag string) bson.D {
 					actualType := val.Kind()
 					// check if val is struct
 					if actualType == reflect.Struct {
-						o := structToBson(val, tag)
-						out = append(out, primitive.E{Key: fieldTag, Value: o})
+						regStructToBson(fieldValue, fieldTag, &out, isUpdate)
 						continue
 					}
 					// if actual type is not struct
@@ -59,12 +91,12 @@ func structToBson(elements reflect.Value, tag string) bson.D {
 	return out
 }
 
-func StructToBson(from interface{}, tags ...string) bson.D {
+func StructToBson(from interface{}, isUpdate bool, tags ...string) bson.D {
 	tag := "bson"
 	if len(tags) > 0 {
 		tag = tags[0]
 	}
 	elems := reflect.ValueOf(from).Elem()
-	out := structToBson(elems, tag)
+	out := structToBson(elems, isUpdate, tag)
 	return out
 }
